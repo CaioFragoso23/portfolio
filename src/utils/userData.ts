77272fb06bf101a6945d40d629c89edc6bb818de
CaioFragoso23 <caiocesarfragoso@gmail.com>
2023-05-16 15:41:54 -0300

    Alterando configurações pessoais para o PortFolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Caio Fragoso",
+  githubUser: "CaioFragoso23",
+  linkedinUser: "caiofragoso23",
+  whatsappNumber: "+5521992583062",
 };
